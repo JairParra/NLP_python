fb32826284a53447dd44cfc4cc153bed6c588e9f
Hair Parra <jair.parra@outlook.com>
2020-05-21 16:49:44 -0400

    Add files via upload

---
+++ Multioutput_prediction.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 20:41:03 2019
+
+@author: jairp
+"""
+
+### *** Multioutput Prediction ###  
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+df = pd.read_csv('./Datasets/handcoding_once_only.csv')
+
+# ?????? 
+
+
+
+
+
+
+
+
